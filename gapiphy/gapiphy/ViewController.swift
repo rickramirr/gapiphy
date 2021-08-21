a0828ff381f8aa96a19e4c2a8b12b2241dc7a5db
Ricardo Ramirez <ricardo.ramirezr@elektra.com.mx>
2021-08-21 13:00:45 -0500

    new xcode project

---
+++ gapiphy/gapiphy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gapiphy
+//
+//  Created by Ricardo Ramirez on 20/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
