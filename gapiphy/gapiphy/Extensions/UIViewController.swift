0e996ce0d9f96e395788184ddc3be91c8909bdda
Ricardo Ramirez <ricardo.ramirezr@elektra.com.mx>
2021-08-23 11:18:56 -0500

    new files

---
+++ gapiphy/gapiphy/Extensions/UIViewController.swift
@@ -0,0 +1,8 @@
+//
+//  UIViewController.swift
+//  gapiphy
+//
+//  Created by Ricardo Ramirez on 23/08/21.
+//
+
+import Foundation
---
+++ gapiphy/gapiphy/LoadingViewController.swift
@@ -0,0 +1,8 @@
+//
+//  LoadingViewController.swift
+//  gapiphy
+//
+//  Created by Ricardo Ramirez on 23/08/21.
+//
+
+import Foundation
